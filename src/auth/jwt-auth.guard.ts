be48acc746d590911910d057bd3ccb66335a73ed
Andres Zambra Sazo <extazambra@transbank.cl>
2026-06-24 16:02:19 -0400

    avances

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,5 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private config: ConfigService) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    if (this.config.get<string>('AUTH_ENABLED', 'true') === 'false') {
+      return true;
+    }
+    return super.canActivate(context);
+  }
+}
